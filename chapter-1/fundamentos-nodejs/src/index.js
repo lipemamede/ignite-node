0e172f6e87d2dcbbc4c6f9f56fdc7f7f8fd504fc
Felipe Mamede <felipebrasilmv@hotmail.com>
2021-03-12 14:50:09 -0300

    Finalizando modulo de fundamentos nodejs

---
+++ chapter-1/fundamentos-nodejs/src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/courses", (req, res) => {
+    const query = req.query;
+    console.log(query);
+    return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (req, res) => {
+    const body = req.body;
+    console.log(body);
+    return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id",  (req, res) => {
+    const params = req.params;
+    console.log(params);
+    return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id",  (req, res) => {
+    return res.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id",  (req, res) => {
+    return res.json(["Curso 6", "Curso 7", "Curso 4"]);
+});
+
+app.listen(3333, () => {
+    console.log("Server started at port 3333! 🚀🚀");
+});
