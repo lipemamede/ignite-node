d9bb51600dd6d893d3b6bcca8038008fbc3f53c6
Felipe Mamede <felipebrasilmv@hotmail.com>
2021-07-17 22:02:45 -0300

    separando os repositórios

--- chapter-2/rentalx/src/routes/categories.routes.ts
+++ chapter-2/rentalx/src/routes/categories.routes.ts
@@ -1,19 +1,16 @@
 import { Router } from 'express'
 
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
-
-categoriesRoutes.get('/', (request, response) => {
-  const all = categoriesRepository.list();
-
-  return response.json(all);
-})
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoryController.handle(request, response);
+})
+
+categoriesRoutes.get('/', (request, response) => {
+  return listCategoriesController.handle(request, response);
 })
 
 export { categoriesRoutes };
